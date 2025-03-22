5445bea6d50efb72af897e99572ca63e955a3d1d
RohitBMalviya <rohitmalviya@gmail.com>
2025-03-22 14:56:42 +0530

    app name of django change:

--- django_project_ecommerce/django_app_ecommerce/apps.py
+++ django_project_ecommerce/django_app_ecommerce/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoProjectEcommerceAppConfig(AppConfig):
+class DjangoAppEcommerceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_project_ecommerce_app"
+    name = "django_app_ecommerce"
